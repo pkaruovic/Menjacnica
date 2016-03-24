d9befd44228141b5e372cbae5b2fb7afd3d9c3b3
pkaruovic <pkaruovic@gmail.com>
2016-03-24 21:08:49 +0100

    Dodate metode toString, hashCode i equals

--- Menjacnica/src/valute/Kurs.java
+++ Menjacnica/src/valute/Kurs.java
@@ -9,6 +9,50 @@ public class Kurs {
 	private double prodajniKurs;
 	private double srednjiKurs;
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dan == null) ? 0 : dan.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(prodajniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(srednjiKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Kurs other = (Kurs) obj;
+		if (dan == null) {
+			if (other.dan != null)
+				return false;
+		} else if (!dan.equals(other.dan))
+			return false;
+		if (Double.doubleToLongBits(kupovniKurs) != Double.doubleToLongBits(other.kupovniKurs))
+			return false;
+		if (Double.doubleToLongBits(prodajniKurs) != Double.doubleToLongBits(other.prodajniKurs))
+			return false;
+		if (Double.doubleToLongBits(srednjiKurs) != Double.doubleToLongBits(other.srednjiKurs))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Kurs [dan=" + dan + ", kupovniKurs=" + kupovniKurs + ", prodajniKurs=" + prodajniKurs + ", srednjiKurs="
+				+ srednjiKurs + "]";
+	}
+
 	public GregorianCalendar getDan() {
 		return dan;
 	}
