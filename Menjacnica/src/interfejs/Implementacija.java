52c3040f978a0cdacb65ed38022f0272fbf25cf5
pkaruovic <pkaruovic@gmail.com>
2016-03-24 21:53:05 +0100

    Implementirana klasa Implementacija u master grani

--- Menjacnica/src/interfejs/Implementacija.java
+++ Menjacnica/src/interfejs/Implementacija.java
@@ -1,14 +1,34 @@
 package interfejs;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import valute.Kurs;
 
 public class Implementacija implements MenjacnicaInterfejs {
+	
+	private LinkedList<Kurs> kursevi;
+	
+	public Implementacija() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Implementacija(LinkedList<Kurs> kursevi) {
+		
+		this.kursevi = kursevi;
+	}
 
 	@Override
 	public void dodajKursValute(GregorianCalendar dan, double prodajniKurs, double kupovniKurs, double srednjiKurs) {
-		// TODO Auto-generated method stub
+		
+		Kurs kurs = new Kurs();
+		
+		kurs.setProdajniKurs(prodajniKurs);
+		kurs.setKupovniKurs(kupovniKurs);
+		kurs.setSrednjiKurs(srednjiKurs);
+		kurs.setDan(dan);
+		
+		kursevi.add(kurs);
 
 	}
 
@@ -23,5 +43,15 @@ public class Implementacija implements MenjacnicaInterfejs {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	public LinkedList<Kurs> getKursevi() {
+		return kursevi;
+	}
+
+	public void setKursevi(LinkedList<Kurs> kursevi) {
+		this.kursevi = kursevi;
+	}
+	
+	
 
 }
