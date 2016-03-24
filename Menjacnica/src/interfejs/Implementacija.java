43c187987badb059e1f5351e98b52f8abaa03cc1
pkaruovic <pkaruovic@gmail.com>
2016-03-24 21:40:21 +0100

    Napravljena klasa Implementacija

---
+++ Menjacnica/src/interfejs/Implementacija.java
@@ -0,0 +1,27 @@
+package interfejs;
+
+import java.util.GregorianCalendar;
+
+import valute.Kurs;
+
+public class Implementacija implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajKursValute(GregorianCalendar dan, double prodajniKurs, double kupovniKurs, double srednjiKurs) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izbrisiKursValute(GregorianCalendar dan) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Kurs pronadjiKursValute(GregorianCalendar dan) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
