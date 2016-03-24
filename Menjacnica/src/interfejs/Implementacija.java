f9d993047d64fc25fd5d49fb96d885629af133a0
pkaruovic <pkaruovic@gmail.com>
2016-03-24 21:48:01 +0100

    Implementirane metode klase Implementacija

--- Menjacnica/src/interfejs/Implementacija.java
+++ Menjacnica/src/interfejs/Implementacija.java
@@ -1,27 +1,65 @@
 package interfejs;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import valute.Kurs;
 
 public class Implementacija implements MenjacnicaInterfejs {
+	
+	private LinkedList<Kurs> kursevi;
+	
+	public Implementacija() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Implementacija(LinkedList<Kurs> kursevi) {
+		this.kursevi = kursevi;
+	}
 
 	@Override
 	public void dodajKursValute(GregorianCalendar dan, double prodajniKurs, double kupovniKurs, double srednjiKurs) {
-		// TODO Auto-generated method stub
+		Kurs k = new Kurs();
+		
+		k.setDan(dan);
+		k.setKupovniKurs(kupovniKurs);
+		k.setProdajniKurs(prodajniKurs);
+		k.setSrednjiKurs(srednjiKurs);
+		
+		kursevi.add(k);
 
 	}
 
 	@Override
 	public void izbrisiKursValute(GregorianCalendar dan) {
-		// TODO Auto-generated method stub
+		
+		for(int i=0; i<kursevi.size(); i++){
+			if(kursevi.get(i).getDan().equals(dan)){
+				kursevi.remove(i);
+			}
+		}
 
 	}
 
 	@Override
 	public Kurs pronadjiKursValute(GregorianCalendar dan) {
-		// TODO Auto-generated method stub
+		
+		for(int i=0; i<kursevi.size(); i++){
+			if(kursevi.get(i).getDan().equals(dan)){
+				return kursevi.get(i);
+			}
+		}
+		
 		return null;
 	}
 
+	public LinkedList<Kurs> getKursevi() {
+		return kursevi;
+	}
+
+	public void setKursevi(LinkedList<Kurs> kursevi) {
+		this.kursevi = kursevi;
+	}
+
+	
 }
